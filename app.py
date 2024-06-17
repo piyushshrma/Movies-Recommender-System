8f640a9805158a54e3c2bdf4de96f44b81c7ce9a
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2024-06-17 14:16:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,20 +23,24 @@ def recommend(movie):
 
     return recommended_movie_names,recommended_movie_posters
 
+st.markdown("<h1 style='text-align: center; color: black;'>Movie Recommender System</h1>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; color: black;'>Find a similar movie from a dataset of 5,000 movies!</h4>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; color: black;'>Web App created by Sagar Bapodara</h4>", unsafe_allow_html=True)
 
-st.header('Movie Recommender System')
+
 movies = pickle.load(open('movies.pkl','rb'))
 similarity = pickle.load(open('similarity.pkl','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
+    "Type or select a movie you like :",
     movie_list
 )
 
 if st.button('Show Recommendation'):
+    st.write("Recommended Movies based on your interests are :")
     recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_movie_names[0])
         st.image(recommended_movie_posters[0])
@@ -54,6 +58,5 @@ if st.button('Show Recommendation'):
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
 
-
-
-
+st.title(" ")
+# st.write("The code for this recommender system is available [here](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
